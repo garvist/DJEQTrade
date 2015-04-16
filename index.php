570ff9aefd7f3864284301ce39cd77b4fef8acdd
Warren S <mr.dude1848@gmail.com>
2015-04-16 01:01:55 -0400

    Added mockup4

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>DJ Equipment Trade</title>
+		<link href="/Static/main.css" rel="stylesheet" type="text/css" />
+	</head>
+	
+	<body>
+		<div class="header">
+			<ul class="nav">
+				<!-- logo -->
+				<li class="logo">
+					<span class="logo-dj">DJ</span><!--
+					--><span class="logo-eq">eq</span><!--
+					--><span class="logo-x">X</span>
+				</li>
+				
+				<!-- nav options -->
+				<li>Logout</li>
+				<li>Messages</li>
+				<li>Post an ad</li>
+				<li>View ads</li>
+			</ul>
+			
+			<form>
+				<input type="text" placeholder="search" name="search" />
+			</form>
+		</div>
+		
+		<div class="page">
+			<div class="ad">
+				<h1 class="ad-title">Jimbo's Amazing Speakers</h1>
+				<div class="ad-meta">
+					<span>Categories: speakers</span>
+					<span>Tags: tags-r-fun, high-range</span>
+				</div>
+				
+				<p class="ad-description">
+					I'm selling my old speakers. Message me with offers.
+				</p>
+				
+				<img class="ad-image" src="/blownspeakers.jpg" />
+				
+				<span class="ad-byline">Posted by DJ Jimbo on Apr 2 @ 4:30pm</span>
+			</div>
+			
+			<div class="ad">
+				<h1 class="ad-title">Jimbo's Amazing Subwoofers</h1>
+				<div class="ad-meta">
+					<span class="ad-author">Posted by DJ Jimbo</span>
+					<span>Categories: speakers</span>
+					<span>Tags: tags-r-fun, high-range</span>
+				</div>
+				
+				<p class="ad-description">
+					Check out my awesome subwoofers! Message me with offers.
+				</p>
+				
+				<img class="ad-image" src="/blownspeakers.jpg" />
+				
+				<span class="ad-byline">Posted by DJ Jimbo on Apr 2 @ 4:37pm</span>
+			</div>
+		</div>
+	</body>
+</html>
