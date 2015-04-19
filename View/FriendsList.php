b63da25b0650c32af4cca5a797b095c2340195aa
KarlPreisner <karlpreisner@gwu.edu>
2015-04-18 23:49:13 -0400

    Added friendslist.css and FriendsList.php

---
+++ View/FriendsList.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'View/View.php'; //defines the View class
+
+class FriendsListView extends View
+{
+	/** Returns an array of CSS files that this view needs */
+	public function requiredCSS()
+	{
+		//update css
+		return ["/Static/main.css", "/Static/friendslist.css"];
+	}
+	
+	/** Outputs this view's HTML */
+	public function outputHTML()
+	{
+		//$searchterm = ( isset($_GET['s']) ? $_GET['s'] : '' );
+		
+		echo "<div class=\"page\">";
+		echo "	<div class=\"friend\">";
+		echo "		<h1 class=\"ad-title\">You have no friends</h1>";
+		echo "  </div>";
+		echo "	<body><p>This is a description of your friend.</p></body>"
+		echo "</div>";
+	}
+}
+
+?>
