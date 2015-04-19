d146a6096595a60cd0b73a60278787cad0c130cb
KarlPreisner <karlpreisner@gwu.edu>
2015-04-19 01:38:31 -0400

    still trying

--- View/navigation.php
+++ View/navigation.php
@@ -29,7 +29,7 @@ class NavigationView extends View
 		echo "		</li>";
 		echo "";
 		echo "		<!-- nav options -->";
-		echo "		<li><a href=\"Controller\homepage.php\">Homepage</a></li>";
+		echo "		<li><a href=\"index.php\">Homepage</a></li>";
 		echo "		<li>Post an ad</li>";
 		echo "		<li>View ads</li>";
 		echo "		<li>Friends</li>";
