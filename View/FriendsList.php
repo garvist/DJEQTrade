c5a02c28240f4bb80209e5e3b8ecf8d0576a5cda
KarlPreisner <karlpreisner@gwu.edu>
2015-04-20 00:26:34 -0400

    Commented out all friendslist.css

--- View/FriendsList.php
+++ View/FriendsList.php
@@ -25,6 +25,7 @@ class FriendsListView extends View
 		echo "			<li><a href=\"/?c=post an ad\">Adam Sandler (currently has a link to \"post an ad\")</a></li>"; //link to a general friends page, yet to be created.
 		echo "			<li>Brad pitt</li>";
 		echo "			<li>Emil Gilles</li>";
+		echo "			<li>Orlando Bloom</li>";
 		echo "		</ul>";
 		echo "  </div>";
 		echo "</div>";
