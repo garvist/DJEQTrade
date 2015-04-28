b6dccb54b7ad4333f48f0d1c98dad81d425df4cf
Warren S <mr.dude1848@gmail.com>
2015-04-27 22:02:56 -0400

    Moved reviews into aboutMe view

--- View/aboutMe.php
+++ View/aboutMe.php
@@ -16,7 +16,7 @@ class AboutMeView extends View
 	/** Returns an array of CSS files that this view needs */
 	public function requiredCSS()
 	{
-		return ["/Static/main.css","/Static/aboutMe.css"]; //return an array containing the string "/Static/main.css" and "/Static/postlist.css"
+		return ["/Static/main.css","/Static/aboutMe.css", "/Static/reviewlist.css"]; //return an array containing the string "/Static/main.css" and "/Static/postlist.css"
 	}
 	
 	/** Outputs this view's HTML */
@@ -70,6 +70,32 @@ class AboutMeView extends View
 		}
 		echo "";
 		echo "	</div>";
+		
+		echo "<div class =\"review-title\">";
+		echo "	<h1>All Reviews</h1>";
+		echo "</div>";
+		echo "<div class=\"review-list\">";
+		
+		foreach( $this->db->getReviewsForUser( $customer_id ) as $review )
+		{
+			/*
+			$review = [ "author_customer_id" => $author_customer_id, "author_first_name" => $author_first_name, "author_last_name" => $author_last_name,
+						"target_customer_id" => $target_customer_id, "target_first_name" => $target_first_name, "target_last_name" => $target_last_name,
+						"review_text" => $review_text, "score" => $score ];
+			*/
+						
+			echo "	<div class=\"review\">";
+			echo "	<h1 class=\"review-title\">Score: {$review['score']}</h1>";
+			echo "	";
+			echo "	<p class=\"review-description\">";
+			echo "	{$review['review_text']}";
+			echo "	</p>";
+			echo "	";
+			echo "	<span class=\"review-byline\">Review written by {$review['author_first_name']} {$review['author_last_name']}</span>";
+		}
+		
+		echo "</div>";
+		
 		echo "</div>";
 	}
 }
